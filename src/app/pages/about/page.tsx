133c2751909f030e66d14b4461816fb332d3dfae
JFarah <joaquinefarah@gmail.com>
2025-06-17 10:42:20 -0300

    Enhance AboutPage layout with improved responsiveness and styling adjustments

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -11,7 +11,7 @@ export default function AboutPage() {
     <div className="bg-gray-50">      
       <Navbar />
       <Whatsapp />
-      <div className="relative h-screen -mr-10">
+      <div className="relative h-screen min-h-screen -mr-10">
         <Image 
           src="/quienes1.JPG" 
           alt="Icono" 
@@ -19,8 +19,32 @@ export default function AboutPage() {
           height={2000} 
           className="absolute top-0 left-0 w-full h-full object-cover shadow-xl brightness-65" 
         />
-        <div className="absolute top-3/5 left-1/5 transform -translate-x-1/5 -translate-y-1 text-white text-4xl font-semibold text-center rounded-lg bg-gray-900/50 p-5 backdrop-blur-xs w-full" >
-          <p className="uppercase" >SOMOS <span style={{ backgroundColor: '#f7950a' }} className=" text-white px-1 rounded">ESPECIALISTAS</span> EN LA BÚSQUEDA Y OBTENCIÓN DEL LÍMITE TÉCNICO </p>
+        <div
+          className="
+            absolute 
+            top-3/5 left-1/5 
+            transform -translate-x-1/5 -translate-y-1
+            text-white 
+            text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold
+            text-center rounded-lg 
+            bg-gray-900/50 
+            p-2 sm:p-4 md:p-5
+            backdrop-blur-xs 
+            w-[95vw] sm:w-4/5 md:w-3/4 lg:w-full
+            max-w-full
+            mx-auto
+            "
+        >
+          <p>
+            SOMOS{" "}
+            <span
+              style={{ backgroundColor: "#f7950a" }}
+              className="text-white px-1 rounded"
+            >
+              ESPECIALISTAS
+            </span>{" "}
+            EN LA BÚSQUEDA Y OBTENCIÓN DEL LÍMITE TÉCNICO
+          </p>
           <p>TANTO DEL PERSONAL COMO DE LAS HERRAMIENTAS Y EQUIPOS</p>
         </div>
       </div>      
